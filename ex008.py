9c12daa2aec37ed80ba167b364466f7ffe30ca10
Gabriel José Batista <ryubiel@gmail.com>
2023-12-16 16:09:41 -0300

    Add files via upload

---
+++ ex008.py
@@ -0,0 +1,8 @@
+m = float(input ('Digite um valor em metros: '))
+km = m / 1000
+hm = m / 100
+dam = m / 10
+dm = m * 10
+cm = m * 100
+mm = m * 1000
+print ('{}m é equivalente à: \n{}km \n{}hm \n{}dam \n{}dm \n{}cm \n{}mm'.format (m, km, hm,dam ,dm, cm, mm))
