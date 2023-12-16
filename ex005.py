4c45e74df5de81419a6507ea6fa7c47087ebbacc
Gabriel José Batista <ryubiel@gmail.com>
2023-12-16 16:03:17 -0300

    Add files via upload

---
+++ ex005.py
@@ -0,0 +1,2 @@
+n = int(input("Digite um número: "))
+print("O número digitado {}, seu sucessor é {} e o antecessor é {}".format(n, n+1, n-1))
---
+++ ex006.py
@@ -0,0 +1,4 @@
+import math
+
+n = int(input("Digite um número: "))
+print("O dobro é {}, o triplo é {} e a raiz quadrada é {:.2f}".format(n*2,n*3,math.sqrt(n)))
